f8e01c78cb07d74e7fee77ef429a387634824a5e
ShrinathNR <nr.shrinath@gmail.com>
2022-02-17 16:01:49 +0530

    added all the math funcs

---
+++ programs/calcdapp/src/lib.rs
@@ -0,0 +1,65 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+
+pub mod calcdapp {
+    use super::*;
+
+    pub fn create(ctx:Context<Create>, init_message:String)->ProgramResult{
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.greetings = init_message;
+        Ok(())
+    }
+    pub fn add(ctx:Context<Addition>, num1: i64, num2: i64)->ProgramResult{
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1+num2;
+        Ok(())
+    }
+    pub fn multily(ctx:Context<Multiplication>, num1: i64, num2: i64)-> ProgramResult{
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1*num2;
+        Ok(())
+    }
+    pub fn subract(ctx: Context<Subraction>, num1: i64, num2: i64)->ProgramResult{
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1-num2;
+        Ok(())
+    }
+    pub fn divide(ctx:Context<Division>, num1:i64, num2: i64)-> ProgramResult{
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1/num2;
+        calculator.remainder = num1%num2;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Addition <'info>{
+    #[account(mut)]
+    pub calculator: Account <'info, Caclulator>
+}
+#[derive(Accounts)]
+pub struct Multiplication <'info>{
+    #[account(mut)]
+    pub calculator : Account <'info, Caclulator>
+}
+#[derive(Accounts)]
+pub struct Subraction <'info>{
+    #[account(mut)]
+    pub calculator : Account <'info, Caclulator>
+}
+
+#[derive(Accounts)]
+pub struct Division <'info>{
+    #[account(mut)]
+    pub calculator : Account <'info, Caclulator>
+}
+
+#[account]
+pub struct Caclulator{
+    pub greetings: String,
+    pub result: i64,
+    pub remainder: i64,
+}
